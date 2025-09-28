8824433e63ee08c891589fc70308bbe2cbdda779
Mohamed Elsayed <elmoosayed@gmail.com>
2025-09-29 02:10:19 +0300

    feat: Implement onboarding view and navigation
    - Created `OnboardingView` as a new stateless widget.
    - Added `onboardingView` route to `Routes` and `AppRouter`.
    - Updated `AnimatedSplashView` to navigate to `OnboardingView` after a delay.
    - Modified `AnimatedSplashView` to be a `StatefulWidget` to handle navigation logic in `initState`.
    - Adjusted image height in `AnimatedSplashView` using `flutter_screenutil`.

--- lib/animated_splash_view.dart
+++ lib/animated_splash_view.dart
@@ -1,10 +1,42 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruit_hub/core/helpers/app_assets.dart';
+import 'package:fruit_hub/features/onboarding/presentation/views/onboarding_view.dart';
 
-class AnimatedSplashView extends StatelessWidget {
+class AnimatedSplashView extends StatefulWidget {
   const AnimatedSplashView({super.key});
+
+  @override
+  State<AnimatedSplashView> createState() => _AnimatedSplashViewState();
+}
+
+class _AnimatedSplashViewState extends State<AnimatedSplashView> {
+  @override
+  void initState() {
+    super.initState();
+    navigate();
+  }
+
+  void navigate() async {
+    Future.delayed(const Duration(seconds: 2, milliseconds: 300), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const OnboardingView(),
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(
+              opacity: CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeInOut,
+              ),
+              child: child,
+            );
+          },
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +55,7 @@ class AnimatedSplashView extends StatelessWidget {
             duration: const Duration(milliseconds: 1200),
             child: Image.asset(
               'assets/images/splash_android_12.png',
-              height: 300,
+              height: 300.h,
             ),
           ),
           FadeInUp(
